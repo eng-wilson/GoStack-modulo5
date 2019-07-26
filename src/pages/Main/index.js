93320d9dff0da470d2c5e750ad71d0ea13d4ed8e
Wilson Carvalho <eng.wilson.c@gmail.com>
2019-07-26 13:51:45 -0300

    chore: exibição de repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,8 +3,9 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
+import Container from '../../Components/Container';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import { Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
   state = {
